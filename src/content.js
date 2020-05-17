5c4ce56a73c3ff0d43d11bfa557690f87869c371
lucram <lucienramassamy@gmail.com>
2020-05-17 15:11:24 +0200

    animation de la page works pour les changement de projets, ne focntionne oas

--- src/content.js
+++ src/content.js
@@ -4,27 +4,37 @@ const project = [
     number: '1',
     date: '2015 à Aujourd\'hui',
     color: '#50dec9',
+    shadow: '#00a489',
+    logo: require('./assets/img/works/logo-coachylive.png'), // eslint-disable-line
   }, {
     title: 'CochyLive',
     number: '2',
     date: '2017',
     color: '#fe9ca9',
+    shadow: '#ed5962',
+    logo: require('./assets/img/works/logo-creamsources.png'), // eslint-disable-line
 
   }, {
     title: 'CreamSources',
     number: '3',
     date: '2016',
     color: '#ffce71',
+    shadow: '#BD913B',
+    logo: require('./assets/img/works/logo-fff.png'), // eslint-disable-line
   }, {
     title: 'FFF',
     number: '4',
     date: '2014',
     color: '#AE72D6',
+    shadow: '#7E529C',
+    logo: require('./assets/img/works/logo-psa.png'), // eslint-disable-line
   }, {
     title: 'Youphil',
     number: '5',
     date: '2012',
     color: '#A2D672',
+    shadow: '#8CB864',
+    logo: require('./assets/img/works/logo-youphil.png'), // eslint-disable-line
   },
 ];
 
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -2,12 +2,17 @@
   <div class="works">
     <p class="title--background"> {{ this.project[this.i].title }} </p>
     <p class="number" :style='numberColor'>{{ this.project[this.i].number }}</p>
+    <p class="number second" :style='shadowColor'>{{ this.project[this.i].number }}</p>
+    <div class="logo"> <img :src="this.project[i].logo" alt="" srcset=""> </div>
     <p class="date">{{ this.project[this.i].date }}</p>
   </div>
 </template>
 
 
 <script>
+// IMPORT of modules
+import gsap from 'gsap';
+// IMPORT of components
 import project from '../content';
 
 export default {
@@ -15,26 +20,40 @@ export default {
   name: 'Works',
   mounted() {
     this.changeProject();
+
+    // this.nextTl.to('.second', 0.5, { top: '50%', left: '50%' }, 'start')
+    //   .to('.logo', 0.5, { rotate: '90deg', opacity: 0 }, 'start')
+    //   .to('.title--background, .date', 0.5, { opacity: 0 }, 'start')
+    //   .to('.number', 0.5, { scale: 0, onComplete: () => { this.i += 1; } })
+    //   .to('.number', 0.5, { scale: 1 })
+    //   .to('.second', 0.5, { top: '51%', left: '51%' }, 'sndStep')
+    //   .to('.title--background, .date', 0.5, { opacity: 1 }, 'sndStep')
+    //   .to('.logo', 0.5, { rotate: '0deg', opacity: 1 });
   },
   data() {
     return {
       project,
       i: 0,
+      nextTl: gsap.timeline({ paused: true }),
     };
   },
   methods: {
     next() {
       if (this.i === this.project.length - 1) {
-        this.i = 0;
+        this.animationProjectChange();
+        // this.i = 0
       } else {
-        this.i += 1;
+        this.animationProjectChange();
+        // this.i += 1
       }
     },
     previous() {
       if (this.i === 0) {
-        this.i = this.project.length - 1;
+        this.animationProjectChange();
+        // this.i = this.project.length - 1
       } else {
-        this.i -= 1;
+        this.animationProjectChange();
+        // this.i -= 1
       }
     },
     onScroll(e) {
@@ -67,12 +86,27 @@ export default {
       document.body.addEventListener('wheel', this.onScroll);
       document.body.addEventListener('keydown', this.onKey);
     },
-
+    animationProjectChange(e) {
+      this.nextTl.to('.second', 0.5, { top: '50%', left: '50%' }, 'start')
+        .to('.logo', 0.5, { rotate: '90deg', opacity: 0 }, 'start')
+        .to('.title--background, .date', 0.5, { opacity: 0 }, 'start')
+        .to('.number', 0.5, { scale: 0, onComplete: () => e })
+        .to('.number', 0.5, { scale: 1 })
+        .to('.second', 0.5, { top: '51%', left: '51%' }, 'sndStep')
+        .to('.title--background, .date', 0.5, { opacity: 1 }, 'sndStep')
+        .to('.logo', 0.5, { rotate: '0deg', opacity: 1 })
+        .play();
+    },
   },
   computed: {
     numberColor() {
       return {
         color: `${this.project[this.i].color}`,
+      };
+    },
+    shadowColor() {
+      return {
+        color: `${this.project[this.i].shadow}`,
       };
     },
   },
@@ -87,11 +121,9 @@ export default {
     display: flex;
     flex-direction: column;
     position: relative;
-    align-items: center;
-    justify-content: center;
     background-color: $grey;
-
     .title--background {
+      cursor: default;
       font-family: Title;
       font-size: 20rem;
       margin: 0;
@@ -102,14 +134,46 @@ export default {
       color: $darkGrey;
       }
     .number {
+      cursor: pointer;
       font-family: TextBold;
-      font-size: 15rem;
-      color: $bluePSA;
+      font-size: 20rem;
       margin: 0;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 3;
+      &.second {
+        top: 51%;
+        left: 51%;
+        z-index: 1;
+      }
+    }
+    .logo {
+      height: 12vh;
+      width: 17vw;
+      position: absolute;
+      top: 35%;
+      left: 55%;
+      background: #fefefe;
+      z-index: 2;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transform-origin: top left;
+      img {
+        display: block;
+        max-width: 100%;
+        max-height: 100%;
+      }
     }
     .date {
       color: $black;
       font-size: 1.3rem;
+      position: absolute;
+      top: 70%;
+      left: 50%;
+      transform: translate(-50%,-50%);
     }
   }
 </style>
